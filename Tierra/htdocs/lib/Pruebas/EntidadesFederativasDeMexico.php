0ea0173cfd269717de8f91988c6576d3bec04105
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-06-08 22:26:37 -0500

    Continué con los Estados de México.

--- Tierra/htdocs/lib/Pruebas/EntidadesFederativasDeMexico.php
+++ Tierra/htdocs/lib/Pruebas/EntidadesFederativasDeMexico.php
@@ -28,57 +28,39 @@ class EntidadesFederativasDeMexico {
 
     static public $estructura = array();
     static public $listado    = array(
-        array('nombre' => 'Distrito Federal', 'capital' => '', 'poblacion' => 8851080, 'fundacion' => '1824-11-18'),
-        array('nombre' => 'Aguascalientes', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''),
-        array('nombre' => '', 'capital' => '', 'poblacion' => , 'fundacion' => ''));
+        array('nombre' => 'Distrito Federal',                'capital' => '',                          'poblacion' =>  8851080, 'fundacion' => '1824-11-18'),
+        array('nombre' => 'Aguascalientes',                  'capital' => 'Aguascalientes',            'poblacion' =>  1184996, 'fundacion' => '1857-02-05'),
+        array('nombre' => 'Baja California',                 'capital' => 'Mexicali',                  'poblacion' =>  3155070, 'fundacion' => '1952-01-16'),
+        array('nombre' => 'Baja California Sur',             'capital' => 'La Paz',                    'poblacion' =>   637026, 'fundacion' => '1974-10-08'),
+        array('nombre' => 'Campeche',                        'capital' => 'San Francisco de Campeche', 'poblacion' =>   822441, 'fundacion' => '1863-04-29'),
+        array('nombre' => 'Chiapas',                         'capital' => 'Tuxtla Gutiérrez',          'poblacion' =>  4796580, 'fundacion' => '1824-09-14'),
+        array('nombre' => 'Chihuahua',                       'capital' => 'Chihuahua',                 'poblacion' =>  3406465, 'fundacion' => '1824-07-06'),
+        array('nombre' => 'Coahuila de Zaragoza',            'capital' => 'Saltillo',                  'poblacion' =>  3055395, 'fundacion' => '1824-05-07'),
+        array('nombre' => 'Colima',                          'capital' => 'Colima',                    'poblacion' =>   650555, 'fundacion' => '1856-12-09'),
+        array('nombre' => 'Durango',                         'capital' => 'Victoria de Durango',       'poblacion' =>  1632934, 'fundacion' => '1824-05-22'),
+        array('nombre' => 'Guanajuato',                      'capital' => 'Guanajuato',                'poblacion' =>  5486372, 'fundacion' => '1823-12-20'),
+        array('nombre' => 'Guerrero',                        'capital' => 'Chilpancingo de los Bravo', 'poblacion' =>  3388768, 'fundacion' => '1849-10-27'),
+        array('nombre' => 'Hidalgo',                         'capital' => 'Pachuca de Soto',           'poblacion' =>  2665018, 'fundacion' => '1869-01-16'),
+        array('nombre' => 'Jalisco',                         'capital' => 'Guadalajara',               'poblacion' =>  7350682, 'fundacion' => '1823-12-23'),
+        array('nombre' => 'México',                          'capital' => 'Toluca de Lerdo',           'poblacion' => 15175862, 'fundacion' => '1823-12-20'),
+        array('nombre' => 'Michoacán de Ocampo',             'capital' => 'Morelia',                   'poblacion' =>  4351037, 'fundacion' => '1823-12-22'),
+        array('nombre' => 'Morelos',                         'capital' => 'Cuernavaca',                'poblacion' =>  1777227, 'fundacion' => '1869-04-17'),
+        array('nombre' => 'Nayarit',                         'capital' => 'Tepic',                     'poblacion' =>  1084979, 'fundacion' => '1917-01-26'),
+        array('nombre' => 'Nuevo León',                      'capital' => 'Monterrey',                 'poblacion' =>  4653458, 'fundacion' => '1824-05-07'),
+        array('nombre' => 'Oaxaca',                          'capital' => 'Oaxaca de Juárez',          'poblacion' =>  3801962, 'fundacion' => '1823-12-21'),
+        array('nombre' => 'Puebla',                          'capital' => 'Puebla de Zaragoza',        'poblacion' =>  5779829, 'fundacion' => '1823-12-21'),
+        array('nombre' => 'Querétaro de Arteaga',            'capital' => 'Querétaro',                 'poblacion' =>  1827937, 'fundacion' => '1823-12-23'),
+        array('nombre' => 'Quintana Roo',                    'capital' => 'Chetumal',                  'poblacion' =>  1325578, 'fundacion' => '1974-10-08'),
+        array('nombre' => 'San Luis Potosí',                 'capital' => 'San Luis Potosí',           'poblacion' =>  2585518, 'fundacion' => '1823-12-22'),
+        array('nombre' => 'Sinaloa',                         'capital' => 'Culiacán Rosales',          'poblacion' =>  2767761, 'fundacion' => '1830-10-14'),
+        array('nombre' => 'Sonora',                          'capital' => 'Hermosillo',                'poblacion' =>  2662480, 'fundacion' => '1824-01-10'),
+        array('nombre' => 'Tabasco',                         'capital' => 'Villahermosa',              'poblacion' =>  2238603, 'fundacion' => '1824-02-07'),
+        array('nombre' => 'Tamaulipas',                      'capital' => 'Ciudad Victoria',           'poblacion' =>  3268554, 'fundacion' => '1824-02-07'),
+        array('nombre' => 'Tlaxcala',                        'capital' => 'Tlaxcala de Xicohténcatl',  'poblacion' =>  1169936, 'fundacion' => '1856-12-09'),
+        array('nombre' => 'Veracruz de Ignacio de la Llave', 'capital' => 'Xalapa-Enríquez',           'poblacion' =>  7643194, 'fundacion' => '1823-12-22'),
+        array('nombre' => 'Yucatán',                         'capital' => 'Mérida',                    'poblacion' =>  1955577, 'fundacion' => '1823-12-23'),
+        array('nombre' => 'Zacatecas',                       'capital' => 'Zacatecas',                 'poblacion' =>  1490668, 'fundacion' => '1823-12-23'));
 
 } // Clase EntidadesFederativasDeMexico
-
-/*
-
-# 	Entidad federativa 	Escudo 	Capital 	Ciudad con
-mayor población 	Superficie (km²)26 	Población (2010)27 	Orden de ingreso
-a la Federación 	Fecha de fundación 	Ubicación
-1 	 	Coat of arms of Mexican Federal District.svg 	—— 	—— 	1,485 	 		28 	Mexico map, MX-DIF.svg
-2 	 	Coat of arms of Aguascalientes.svg 	Aguascalientes 	Aguascalientes 	5,618 	1'184,996 	24 	1857-02-0529 	Mexico map, MX-AGU.svg
-3 	Baja California 	Coat of arms of Baja California.svg 	Mexicali 	Tijuana 	71,446 	3'155,070 	29 	1952-01-1630 	Mexico map, MX-BCN.svg
-4 	Baja California Sur 	Coat of arms of Baja California Sur.svg 	La Paz 	La Paz 	73,922 	637,026 	30 	1974-10-0831 	Mexico map, MX-BCS.svg
-5 	Campeche 	Coat of arms of Campeche.svg 	San Francisco de Campeche 	San Francisco de Campeche 	57,924 	822,441 	25 	1863-04-2932 	Mexico map, MX-CAM.svg
-6 	Chiapas 	Coat of arms of Chiapas.svg 	Tuxtla Gutiérrez 	Tuxtla Gutiérrez 	73,289 	4'796,580 	19 	1824-09-1433 	Mexico map, MX-CHP.svg
-7 	Chihuahua 	Coat of arms of Chihuahua.svg 	Chihuahua 	Ciudad Juárez 	247,455 	3'406,465 	18 	1824-07-0634 	Mexico map, MX-CHH.svg
-8 	Coahuila de Zaragoza2 	Coat of arms of Coahuila.svg 	Saltillo 	Saltillo 	151,563 	3'055,395 	16 	1824-05-0735 	Mexico map, MX-COA.svg
-9 	Colima 	Coat of arms of Colima.svg 	Colima 	Colima 	5,625 	650,555 	23 	1856-12-0936 37 	Mexico map, MX-COL.svg
-10 	Durango 	Coat of arms of Durango.svg 	Victoria de Durango 	Victoria de Durango 	123,451 	1'632,934 	17 	1824-05-2238 	Mexico map, MX-DUR.svg
-11 	Guanajuato 	Coat of arms of Guanajuato.svg 	Guanajuato 	León de los Aldama 	30,608 	5'486,372 	2 	1823-12-2039 	Mexico map, MX-GUA.svg
-12 	Guerrero 	Coat of arms of Guerrero.svg 	Chilpancingo
-de los Bravo 	Acapulco de Juárez 	63,621 	3'388,768 	21 	1849-10-2740 	Mexico map, MX-GRO.svg
-13 	Hidalgo 	Coat of arms of Hidalgo.svg 	Pachuca de Soto 	Pachuca de Soto 	20,846 	2'665,018 	26 	1869-01-1641 	Mexico map, MX-HID.svg
-14 	Jalisco 	Coat of arms of Jalisco.svg 	Guadalajara 	Guadalajara 	78,599 	7'350,682 	9 	1823-12-2342 	Mexico map, MX-JAL.svg
-15 	México 	Coat of arms of Mexico State.svg 	Toluca de Lerdo 	Ecatepec de Morelos 	22,357 	15'175,862 	1 	1823-12-2043 	Mexico map, MX-MEX.svg
-16 	Michoacán de Ocampo 	Coat of arms of Michoacan.svg 	Morelia 	Morelia 	58,643 	4'351,037 	5 	1823-12-2244 	Mexico map, MX-MIC.svg
-17 	Morelos 	Coat of arms of Morelos.svg 	Cuernavaca 	Cuernavaca 	4,893 	1'777,227 	27 	1869-04-1745 	Mexico map, MX-MOR.svg
-18 	Nayarit 	Coat of arms of Nayarit.svg 	Tepic 	Tepic 	27,815 	1'084,979 	28 	1917-01-2646 	Mexico map, MX-NAY.svg
-19 	Nuevo León 	Coat of arms of Nuevo Leon.svg 	Monterrey 	Monterrey 	64,220 	4'653,458 	15 	1824-05-0747 	Mexico map, MX-NLE.svg
-20 	Oaxaca 	Coat of arms of Oaxaca.svg 	Oaxaca de Juárez 	Tuxtepec 	93,793 	3'801,962 	3 	1823-12-2148 	Mexico map, MX-OAX.svg
-21 	Puebla 	Coat of arms of Puebla.svg 	Puebla de Zaragoza 	Puebla de Zaragoza 	34,290 	5'779,829 	4 	1823-12-2149 	Mexico map, MX-PUE.svg
-22 	Querétaro de Arteaga 	Coat of arms of Queretaro.svg 	Querétaro 	Querétaro 	11,684 	1'827,937 	11 	1823-12-2350 	Mexico map, MX-QUE.svg
-23 	Quintana Roo 	Coat of arms of Quintana Roo.svg 	Chetumal 	Cancún 	42,361 	1,325,578 	31 	1974-10-0851 	Mexico map, MX-ROO.svg
-24 	San Luis Potosí 	Coat of arms of San Luis Potosi.svg 	San Luis Potosí 	San Luis Potosí 	60,983 	2'585,518 	6 	1823-12-2252 	Mexico map, MX-SLP.svg
-25 	Sinaloa 	Coat of arms of Sinaloa.svg 	Culiacán Rosales 	Culiacán Rosales 	57,377 	2'767,761 	20 	1830-10-1453 	Mexico map, MX-SIN.svg
-26 	Sonora3 	Coat of arms of Sonora.svg 	Hermosillo 	Hermosillo 	179,503 	2'662,480 	12 	1824-01-1054 	Mexico map, MX-SON.svg
-27 	Tabasco 	Coat of arms of Tabasco.svg 	Villahermosa 	Villahermosa 	24,738 	2'238,603 	13 	1824-02-0755 	Mexico map, MX-TAB.svg
-28 	Tamaulipas 	Coat of arms of Tamaulipas.svg 	Ciudad Victoria 	Reynosa 	80,175 	3'268,554 	14 	1824-02-0756 	Mexico map, MX-TAM.svg
-29 	Tlaxcala 	Coat of arms of Tlaxcala.svg 	Tlaxcala de Xicohténcatl 	Tlaxcala de Xicohténcatl 	4,016 	1'169,936 	22 	1856-12-0957 	Mexico map, MX-TLA.svg
-30 	Veracruz de Ignacio de la Llave 	Coat of arms of Veracruz.svg 	Xalapa-Enríquez 	Veracruz 	71,820 	7'643,194 	7 	1823-12-2258 	Mexico map, MX-VER.svg
-31 	Yucatán4 	Coat of arms of Yucatan.svg 	Mérida 	Mérida 	39,612 	1'955,577 	8 	1823-12-2359 	Mexico map, MX-YUC.svg
-32 	Zacatecas 	Coat of arms of Zacatecas.svg 	Zacatecas 	Zacatecas 	75,539 	1'490,668 	10 	1823-12-2360
-
- */
 
 ?>
