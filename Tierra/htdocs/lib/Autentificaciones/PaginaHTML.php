29e83a76fa0efd05d4d03d4f185c8946b9e92c17
Guillermo Valdes Lozano <guivaloz@movimientolibre.com>
2015-06-17 16:25:57 -0500

    Agregué archivos de la sección Sistema.

---
+++ Tierra/htdocs/lib/Autentificaciones/PaginaHTML.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * GenesisPHP - Autentificaciones
+ *
+ * Copyright (C) 2015 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package GenesisPHP
+ */
+
+namespace Autentificaciones;
+
+/**
+ * Clase PaginaHTML
+ */
+class PaginaHTML extends \Base\PaginaHTML {
+
+    /**
+     * Constructor
+     */
+    public function __construct() {
+        parent::__construct('autentificaciones');
+    } // constructor
+
+    /**
+     * HTML
+     *
+     * @return string Código HTML
+     */
+    public function html() {
+        // Ejecutar este método en el padre
+        return parent::html();
+    } // html
+
+} // Clase PaginaHTML
+
+?>
