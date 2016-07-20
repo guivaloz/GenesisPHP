a16d83c6a0ebfd79ba8dfde232346dc11e6f2d2c
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2016-07-20 11:39:31 -0500

    Cambios en MensajeWeb e impresion.

---
+++ Eva/htdocs/lib/Base2/TemaImpresion.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * GenesisPHP - TemaImpresion
+ *
+ * Copyright (C) 2016 Guillermo Valdés Lozano
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * @package GenesisPHP
+ */
+
+namespace Base2;
+
+/**
+ * Clase abtracta TemaImpresion
+ */
+abstract class TemaImpresion {
+
+    public $sistema;               // Nombre del sistema
+    public $titulo;                // Título de la página
+    public $css;                   // Ruta al archivo CSS
+    public $contenido  = array();  // Arreglo o texto con el contenido
+    public $javascript = array();  // Arreglo o texto con Javascript
+
+    /**
+     * Cabecera HTML
+     */
+    abstract protected function cabecera_html();
+
+    /**
+     * Final HTML
+     */
+    abstract protected function final_html();
+
+    /**
+     * HTML
+     */
+    abstract public function html();
+
+} // Clase abtracta TemaImpresion
+
+?>
