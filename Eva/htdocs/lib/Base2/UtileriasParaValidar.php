37788b34fed0d1e4818e3b06fc5590e4d4898115
Guillermo Valdés Lozano <guivaloz@movimientolibre.com>
2017-05-10 11:51:11 -0500

    Agregué signos de admiración a la validación de frase y notas.

--- Eva/htdocs/lib/Base2/UtileriasParaValidar.php
+++ Eva/htdocs/lib/Base2/UtileriasParaValidar.php
@@ -70,7 +70,7 @@ abstract class UtileriasParaValidar {
     public static function validar_frase($frase) {
         if (trim($frase) == '') {
             return FALSE;
-        } elseif (preg_match('/^[a-zA-Z0-9áÁéÉíÍóÓúÚüÜñÑ#&%$@¿?()<>"“” .,;:\'\/\[\]*+_-]+$/', $frase)) {
+        } elseif (preg_match('/^[a-zA-Z0-9áÁéÉíÍóÓúÚüÜñÑ#&%$@¿?¡!()<>"“” .,;:\'\/\[\]*+_-]+$/', $frase)) {
             return TRUE;
         } else {
             return FALSE;
@@ -86,7 +86,7 @@ abstract class UtileriasParaValidar {
     public static function validar_notas($notas) {
         if (trim($notas) == '') {
             return FALSE;
-        } elseif (preg_match('/^[a-zA-Z0-9áÁéÉíÍóÓúÚüÜñÑ#&%$@¿?()<>{}|="“” .,:;\'\/\n\r\[\]\\\\_+*-]+$/', $notas)) {
+        } elseif (preg_match('/^[a-zA-Z0-9áÁéÉíÍóÓúÚüÜñÑ#&%$@¿?¡!()<>{}|="“” .,:;\'\/\n\r\[\]\\\\_+*-]+$/', $notas)) {
             return TRUE;
         } else {
             return FALSE;
